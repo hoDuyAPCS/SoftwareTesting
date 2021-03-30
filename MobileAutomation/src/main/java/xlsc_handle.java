1cd29233610072702abc41edd44026d1cdf156b2
Ho Duy <40297016+hoDuyAPCS@users.noreply.github.com>
2021-03-30 09:39:07 +0700

    Add files via upload

---
+++ MobileAutomation/src/main/java/xlsc_handle.java
@@ -0,0 +1,58 @@
+//Apache POI (Poor Obfuscation Implementation) : Java API for reading and writing Microsoft Document
+//Download common-collections4-4.1.jar
+//Download poi-3.17.jar
+//Download poi-ooxml-3.17.jar
+//Download poi-ooxml-schemas-3.17.jar
+//Download xmlbeans-2.6.0.jar Click Here
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.FormulaEvaluator;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class xlsc_handle {
+    public static void main(String[] args) throws IOException {
+        try
+        {
+            File file = new File("D:\\SOFTWARE_TESTING\\MobileAutomation\\testdata.xlsx");   //creating a new file instance
+            FileInputStream fis = new FileInputStream(file);   //obtaining bytes from the file
+            //creating Workbook instance that refers to .xlsx file
+            XSSFWorkbook wb = new XSSFWorkbook(fis);
+            XSSFSheet sheet = wb.getSheetAt(0);     //creating a Sheet object to retrieve object
+            Iterator<Row> itr = sheet.iterator();    //iterating over excel file
+            while (itr.hasNext())
+            {
+                Row row = itr.next();
+                Iterator<Cell> cellIterator = row.cellIterator();   //iterating over each column
+                while (cellIterator.hasNext())
+                {
+                    Cell cell = cellIterator.next();
+                    switch (cell.getCellType())
+                    {
+                        case Cell.CELL_TYPE_STRING:    //field that represents string cell type
+                            System.out.print(cell.getStringCellValue() + "\t\t\t");
+                            break;
+                        case Cell.CELL_TYPE_NUMERIC:    //field that represents number cell type
+                            System.out.print(cell.getNumericCellValue() + "\t\t\t");
+                            break;
+                        default:
+                    }
+                }
+                System.out.println("");
+            }
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
